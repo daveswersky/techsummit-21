7881b81df3b1f4ab08b52174f3f671e319a7cb9b
dswersky <dswersky@gmail.com>
2021-02-26 10:04:05 -0600

    starting 3_pipeline

---
+++ 3_pipeline/instance_group.tf
@@ -0,0 +1,27 @@
+## RESOURCE MODULES
+resource "google_compute_subnetwork" "default" {
+  name                     = "mig-subnet"
+  ip_cidr_range            = "10.127.0.0/20"
+  network                  = "default"
+  region                   = var.region
+  private_ip_google_access = true
+}
+
+module "instance_template" {
+  source             = "terraform-google-modules/vm/google//modules/instance_template"
+  version            = "6.1.0"
+  project_id         = var.project_id
+  subnetwork         = google_compute_subnetwork.default.self_link
+  service_account    = var.service_account
+  subnetwork_project = var.project_id
+}
+
+module "vm_mig" {
+  source            = "terraform-google-modules/vm/google//modules/mig"
+  version           = "6.1.0"
+  project_id        = var.project_id
+  region            = var.region
+  target_size       = var.target_size
+  hostname          = "mig-simple"
+  instance_template = module.instance_template.self_link
+}
---
+++ 3_pipeline/variables.tf
@@ -0,0 +1,29 @@
+## VARIABLES
+variable "project_id" {
+  description = "The GCP project to use for integration tests"
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region to create and test resources in"
+  type        = string
+}
+
+variable "zone" {
+  description = "The GCP zone to create and test resources in"
+  type        = string
+}
+
+variable "target_size" {
+  description = "The target number of running instances for this managed instance group. This value should always be explicitly set unless this resource is attached to an autoscaler, in which case it should never be set."
+  default = 1
+}
+
+variable "service_account" {
+  default = null
+  type = object({
+    email  = string
+    scopes = set(string)
+  })
+  description = "Service account to attach to the instance. See https://www.terraform.io/docs/providers/google/r/compute_instance_template.html#service_account."
+}
